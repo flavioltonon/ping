0cc05bfa22fe7a90c40735f8103fa32862bf6e2b
Flávio Tonon <flavioltonon@gmail.com>
2023-10-13 09:56:50 -0300

    feat: add logs

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("got request")
+
 		w.Write([]byte("Pong"))
 	})
 
-	http.ListenAndServe(":8080", mux)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Printf("listening and serving at %s\n", server.Addr)
+
+	server.ListenAndServe()
 }
